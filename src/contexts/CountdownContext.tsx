a056d345ba1389a979a1b92c3d85d2b98f71c8d0
Nelson Araújo <nelsonsantosaraujo@hotmail.com>
2021-02-26 11:55:42 -0300

    chore: Change coutdown timer

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -20,7 +20,7 @@ export const CountdownContext = createContext({} as ICountdownContextData);
 
 export function CountdownProvider({children}: ICountdownProviderProps){
   const { startNewChallenge } = useContext(ChallengesContext);
-  const [time, setTime] = useState(0.1 * 60);
+  const [time, setTime] = useState(25 * 60);
   const [isActive, setIsActive] = useState(false);
   const [hasFinished, setHasFinished] = useState(false);
 
@@ -34,7 +34,7 @@ export function CountdownProvider({children}: ICountdownProviderProps){
   function resetCountDown() {
     clearTimeout(countdownTimeout);
     setIsActive(false);
-    setTime(0.1 * 60);
+    setTime(25 * 60);
     setHasFinished(false);
   }
 
